434560947d3e5ab37a2ac963ed1e83aaa16769e2
a.zelenin <ai.zelenin@yandex.ru>
2022-07-19 18:13:58 +0300

    add static

--- cmd/h-server/main.go
+++ cmd/h-server/main.go
@@ -11,21 +11,17 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
-	err := http.ListenAndServe(net.JoinHostPort("", port), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("addr:%s url:%s", r.RemoteAddr, r.URL)
-		w.Header().Set("Content-Type", "text/css")
-		io.WriteString(w, `
-.red {
-    font-family: sans-serif;
-    line-height: 1.15;
-	color:red;
-}`)
-	}))
+	err := http.ListenAndServe(net.JoinHostPort("", port), LogMiddleware(http.FileServer(http.Dir("./static"))))
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
+func LogMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("addr:%s url:%s", r.RemoteAddr, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
 func handleBody(br io.ReadCloser, cb func(body []byte) error) error {
 	body, err := ioutil.ReadAll(br)
 	if err != nil {
